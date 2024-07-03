e5b73413cf9a61313b32f30bea4c6b4a84dff81d
Antonio Todisco <96833999+AntonioTodisco@users.noreply.github.com>
2024-07-03 15:19:28 +0200

    upload script

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get setup script abspath.
+script_path=$(readlink -f "$0")
+
+# Get dirname from script abspath.
+script_dir=$(dirname "$script_path")
+
+# Set virtual environment name.
+env_name="env"
+
+# Get env name.
+env_path="${script_dir}/${env_name}"
+
+echo "[START] Start 'PrivacyVisionAI'..."
+${env_path}/bin/python3 $script_dir/GUI.py
